c6cebcc3d7999cd61505c52096b74cfd1de5f337
sebastien <sebastien>
2005-06-03 09:20:27 +0000

    [BUG] : create_db

--- install.php
+++ install.php
@@ -18,7 +18,7 @@
 // | Author: Devin Doucette <darksnoopy@shaw.ca> (for archives classes)   |
 // +----------------------------------------------------------------------+
 //
-// $Id: install.php,v 1.4 2005/06/03 09:16:35 sebastien Exp $
+// $Id: install.php,v 1.5 2005/06/03 09:20:27 sebastien Exp $
 
 /**
   * PHP page : Automne Installation Manager
@@ -389,8 +389,8 @@ if ($step === 'gpl') {
 			//go to next step
 			$step = 2;
 		} else {
-			//go to google
-			header("Location: http://www.google.com");
+			//go to www.automne.ws
+			header("Location: http://www.automne.ws");
 			exit;
 		}
 	} else {
@@ -409,7 +409,7 @@ if ($step === 'gpl') {
 			<input type="hidden" name="cms_action" value="acceptlicense" />
 			<input type="hidden" name="install_language" value="'.$install_language.'" />
 			<label for="yes"><input id="yes" type="radio" name="license" value="yes" /> '.$stepGPL_yes.'</label><br />
-			<label for="no"><input id="no" type="radio" name="license" value="no" /> '.$stepGPL_no.'</label><br />
+			<label for="no"><input id="no" type="radio" name="license" value="no" selected="selected" /> '.$stepGPL_no.'</label><br />
 			<input type="submit" class="submit" value="'.$label_next.'" />
 		</form>
 		';
@@ -459,11 +459,11 @@ if ($step == 2) {
 					@mysql_close($dblink);
 				} else {
 					//Try to create the DB
-					if (@mysql_query ("CREATE DATABASE '".$_POST["dbname"]."'",$dblink) !== false) {
-						@mysql_close($dblink);
+					if (@mysql_create_db($_POST["dbname"],$dblink) !== false) {
 					} else {
 						$error .= $error_step2_DB_incorrect_name."<br />";
 					}
+					@mysql_close($dblink);
 				}
 			}
 		}
