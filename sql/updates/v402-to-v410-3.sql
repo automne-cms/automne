f181cb11cb4eb71b82bc750097a23e099773a1e3
sebastien <sebastien.pauchet@ws-interactive.fr>
2010-10-11 11:41:10 +0200

    Correct problem on website codename SQL update file

--- sql/updates/v402-to-v410-3.sql
+++ sql/updates/v402-to-v410-3.sql
@@ -1,21 +1,3 @@
-ALTER TABLE `pagesBaseData_public` ADD `codename_pbd` VARCHAR( 20 ) NOT NULL ;
-ALTER TABLE `pagesBaseData_edited` ADD `codename_pbd` VARCHAR( 20 ) NOT NULL ;
-ALTER TABLE `pagesBaseData_deleted` ADD `codename_pbd` VARCHAR( 20 ) NOT NULL ;
-ALTER TABLE `pagesBaseData_archived` ADD `codename_pbd` VARCHAR( 20 ) NOT NULL ;
-
-ALTER TABLE  `pagesBaseData_public` DROP INDEX  `title_pbd` ,
-ADD FULLTEXT  `title_pbd` (
-`title_pbd` ,
-`linkTitle_pbd` ,
-`keywords_pbd` ,
-`description_pbd` ,
-`codename_pbd`
-)
-ALTER TABLE  `pagesBaseData_edited` DROP INDEX  `title_pbd` ,
-ADD FULLTEXT  `title_pbd` (
-`title_pbd` ,
-`linkTitle_pbd` ,
-`keywords_pbd` ,
-`description_pbd` ,
-`codename_pbd`
-)
+ALTER TABLE  `websites` ADD  `codename_web` VARCHAR( 255 ) NOT NULL AFTER  `id_web` ;
+update websites set codename_web='root' where id_web = '1';
+update websites set codename_web=label_web where id_web != '1';
