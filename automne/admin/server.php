7eb218a58b75d184c6b54745bb9c9d738f734757
sebastien-pauchet <seb@automne-cms.org>
2011-04-01 14:39:25 +0200

    Add Expat / LibXML check for XML module.

--- automne/admin/server.php
+++ automne/admin/server.php
@@ -86,6 +86,44 @@ if (!$cms_user->hasAdminClearance(CLEARANCE_ADMINISTRATION_EDITVALIDATEALL)) { /
 	$view->show();
 }
 
+//Return an array of all phpinfo values
+function phpinfo_array($return=false){
+	ob_start();
+	phpinfo(-1);
+	
+	$pi = preg_replace(
+	array('#^.*<body>(.*)</body>.*$#ms', '#<h2>PHP License</h2>.*$#ms',
+	'#<h1>Configuration</h1>#',  "#\r?\n#", "#</(h1|h2|h3|tr)>#", '# +<#',
+	"#[ \t]+#", '#&nbsp;#', '#  +#', '# class=".*?"#', '%&#039;%',
+	'#<tr>(?:.*?)" src="(?:.*?)=(.*?)" alt="PHP Logo" /></a>'
+	.'<h1>PHP Version (.*?)</h1>(?:\n+?)</td></tr>#',
+	'#<h1><a href="(?:.*?)\?=(.*?)">PHP Credits</a></h1>#',
+	'#<tr>(?:.*?)" src="(?:.*?)=(.*?)"(?:.*?)Zend Engine (.*?),(?:.*?)</tr>#',
+	"# +#", '#<tr>#', '#</tr>#'),
+	array('$1', '', '', '', '</$1>' . "\n", '<', ' ', ' ', ' ', '', ' ',
+	'<h2>PHP Configuration</h2>'."\n".'<tr><td>PHP Version</td><td>$2</td></tr>'.
+	"\n".'<tr><td>PHP Egg</td><td>$1</td></tr>',
+	'<tr><td>PHP Credits Egg</td><td>$1</td></tr>',
+	'<tr><td>Zend Engine</td><td>$2</td></tr>' . "\n" .
+	'<tr><td>Zend Egg</td><td>$1</td></tr>', ' ', '%S%', '%E%'),
+	ob_get_clean());
+	
+	$sections = explode('<h2>', strip_tags($pi, '<h2><th><td>'));
+	unset($sections[0]);
+	
+	$pi = array();
+	foreach($sections as $section){
+		$n = substr($section, 0, strpos($section, '</h2>'));
+		preg_match_all(
+		'#%S%(?:<td>(.*?)</td>)?(?:<td>(.*?)</td>)?(?:<td>(.*?)</td>)?%E%#',
+		$section, $askapache, PREG_SET_ORDER);
+		foreach($askapache as $m)
+			$pi[$n][$m[1]]=(!isset($m[3])||@$m[2]==$m[3]) ? @$m[2] : array_slice($m,2);
+	}
+	
+	return ($return === false) ? print_r($pi) : $pi;
+}
+
 //Test all PHP requirements
 
 //Test PHP version
@@ -104,6 +142,20 @@ if (version_compare(PHP_VERSION, "5.2.0") === -1) {
 } else {
 	$content .= '<li class="atm-pic-ok">PHP version OK ('.PHP_VERSION.')</li>';
 }
+
+//XML
+$pi = phpinfo_array(true);
+if (!isset($pi['xml'])) {
+	$content .= '<li class="atm-pic-cancel"><strong style="color:red">Error</strong>, XML extension not installed</li>';
+} else {
+	if (isset($pi['xml']['EXPAT Version'])) {
+		$content .= '<li class="atm-pic-cancel"><strong style="color:red">Error</strong>, XML extension installed with Expat library.</li>';
+	} else {
+		//isset($pi['xml']['libxml2 Version'])
+		$content .= '<li class="atm-pic-ok">XML extension OK</li>';
+	}
+}
+
 //GD
 if (!function_exists('imagecreatefromgif') || !function_exists('imagecreatefromjpeg') || !function_exists('imagecreatefrompng')) {
 	$content .= '<li class="atm-pic-cancel">Error, GD extension not installed</li>';
@@ -163,7 +215,7 @@ $randomFile = PATH_REALROOT_FS.'/test_'.md5(mt_rand().microtime()).'.tmp';
 if (!is_writable(PATH_REALROOT_FS)) {
 	$content .= '<li class="atm-pic-cancel">Error, No permissions to write files on website root directory ('.PATH_REALROOT_FS.')</li>';
 } else {
-	$content .= '<li class="atm-pic-ok">Website root filesystem permissions are OK</li>';
+	$content .= '<li class="atm-pic-ok">File system write permissions are OK</li>';
 }
 //Email
 if (!@mail("root@localhost", "Automne SMTP Test", "Automne SMTP Test")) {
