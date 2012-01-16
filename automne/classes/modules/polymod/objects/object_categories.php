7d988f54303e4fe24ecd2e82885c88330e84f1cf
sebastien-pauchet <seb@automne-cms.org>
2012-01-16 11:40:15 +0100

    Correct a bug in object categories administration: missing scrollbars.

--- automne/classes/modules/polymod/objects/object_categories.php
+++ automne/classes/modules/polymod/objects/object_categories.php
@@ -285,7 +285,7 @@ class CMS_object_categories extends CMS_object_common
 			$field['id'] 				= 'tree-'.$fieldId;
 			$field['xtype'] 			= 'treepanel';
 			$field['height'] 			= io::isPositiveInteger($params['selectHeight']) ? $params['selectHeight'] : 150;
-			$field['width'] 			= io::isPositiveInteger($params['selectWidth']) ? $params['selectWidth'] : '1000';
+			$field['width'] 			= io::isPositiveInteger($params['selectWidth']) ? $params['selectWidth'] : '100%';
 			$field['autoScroll'] 		= true;
 	        $field['animate'] 			= true;
 	        $field['containerScroll'] 	= true;
